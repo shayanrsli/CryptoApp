808336909a68cc0b05f885d1a5dc2ca3da18bacb
shayanrsli <ubunturasooli@gmail.com>
2025-09-21 10:59:23 -0700

    getting data from api based on the search

--- src/components/modules/Search.tsx
+++ src/components/modules/Search.tsx
@@ -1,5 +1,6 @@
-import type { Dispatch, SetStateAction } from "react";
+import { useEffect, useState, type Dispatch, type SetStateAction } from "react";
 import type { exchangeType } from "../templates/HomePage";
+import { searchCoin } from "../services/CryptoApi";
 
 
 type currencyProps = {
@@ -8,9 +9,28 @@ type currencyProps = {
 }
 
 export default function Search ({setCurrency , currency} : currencyProps) {
+
+    const [text , setText] = useState<string>("")
+    const [coins , setCoins] = useState<object[]>([])
+
+    useEffect(() => {
+        
+        if (!text) return;
+
+        const search = async () => {
+            const res = await fetch(searchCoin(text))
+            const json = await res.json();
+            console.log(json);
+            if (json.coins) setCoins(json.coins); 
+        }
+
+        search();
+
+    }, [text])
+
   return (
     <div>
-      <input type="text"/>
+      <input type="text" placeholder="search" value={text} onChange={(e) => setText(e.target.value)} />
       <select   value={currency}  onChange={(e) => setCurrency(e.target.value as exchangeType)}>
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
--- src/components/services/CryptoApi.ts
+++ src/components/services/CryptoApi.ts
@@ -2,7 +2,11 @@ import type { exchangeType } from "../templates/HomePage"
 
 const BASE_URL = 'https://api.coingecko.com/api/v3'
 const APIKEY = "CG-82PuhEzZjzEJf52WEXW7YJ7j"
-export const getCoinList = (page : number , currency: exchangeType) => {
-    return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${APIKEY}`
-}
+export const getCoinList = (page : number , currency: exchangeType) => 
+         `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${APIKEY}`
+
+
+
+export const searchCoin = (query : string) => 
+    `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${APIKEY}`
 
